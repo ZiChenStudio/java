a95d5dbe6cdbef0f62cc5445a2e348c2d25930e0
ZiChenStudio <zichenstudio@outlook.com>
2022-12-29 11:47:45 +0800

    Update java

---
+++ study/name/ClassNames/ClassNames.java
@@ -0,0 +1,29 @@
+public class ClassNames {
+    public static void main(String[] args) {
+        String fenGe = "----------------------------------------------------------------";
+
+        System.out.println(fenGe);
+        System.out.println("Java Class Name");
+        System.out.println(fenGe);
+        System.out.println("Java类名命名规范");
+        System.out.println("类名的定义：'public class \"类名\"'");
+        System.out.println("使用时注意:");
+        System.out.println("禁止使用类似public等的关键字和保留字.");
+        System.out.println("禁止以\".\"等特殊符号,但\"_\"和\"$\"是可以的.");
+        System.out.println("不推荐以中文命名类名.");
+        System.out.println("尽量使用驼峰命名的大驼峰法");
+        System.out.println(fenGe);
+        System.out.println("拓展资料:");
+        System.out.println("方法名命名法: 命名规范>方法名");
+        System.out.println("驼峰命名法: 命名规范>驼峰命名");
+        System.out.println(fenGe);
+        System.out.println("参考资料:");
+        System.out.println("https://www.liaoxuefeng.com/wiki/1252599548343744/1255884132971296");
+        System.out.println("https://blog.csdn.net/PursueLuo/article/details/81977438");
+        System.out.println(fenGe);
+        System.out.println("Thanks for your reading!");
+        System.out.println("此项目的Github储存库:");
+        System.out.println("https://github.com/ZiChenStudio/java");
+        System.out.println(fenGe);
+    }
+}
